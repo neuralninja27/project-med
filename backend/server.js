901ac188cb6df62117fce4aa395d2f7c21cea120
pranavsoni1101 <pranavsoni1101@gmail.com>
2021-06-14 19:15:50 +0530

    Successfully connected to db, established a schema andfetched data from the db

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,55 @@
 const express = require('express');
 const mongoose  = require('mongoose');
+const { string } = require('yargs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// connecting to MongoDb
+mongoose.connect('mongodb://localhost/Project-Med', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// This tells us whether mongoose is connected or not
+mongoose.connection.on('connected', () => {
+    console.log("Mongoose is connected!!");
+});
+
+// Defining Schema
+const Schema = mongoose.Schema;
+
+const existingUserSchema = new Schema({
+    userEmail: String,
+    userPassword: String
+});
+
+// Defining Model
+const existingUser = mongoose.model('ExistingUser', existingUserSchema);
+
+// Saving data to the databse
+const data = {
+    userEmail: "pranavsoni1101@gmail.com",
+    userPassword: "1234567890"
+};
+// Instance of the model
+const newExistingUser = new existingUser(data);
+// Saving into the database using .save()
+// newExistingUser.save((error) => {
+//     if(error)
+//         console.log("Something went wrong");
+//     else
+//         console.log("Successfully saved data");
+// })
+
 app.get('/', (req, res) =>{
-    console.log("the / route");
+    existingUser.find({ })
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((error) => {
+            console.log("Error: ", error);
+        })
 })
 
 app.listen(PORT, () => {
