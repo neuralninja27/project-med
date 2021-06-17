da8fd16d2997c2c0f5bda294a5979c9fbdf15614
pranavsoni1101 <pranavsoni1101@gmail.com>
2021-06-17 21:52:08 +0530

    Successfully Saving the new user details into to the databse

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -4,17 +4,21 @@ const SignUp = require('../models/signUp');
 const router = express.Router();
 
 router.post('/sign-up/save', (req, res) =>{
-    // SignUp.find({ })
-    //     .then((data) => {
-    //         res.json(data);
-    //     })
-    //     .catch((error) => {
-    //         console.log("Error: ", error);
-    //     })
-    console.log("Body: ", req.body);
-    res.json({
-        msg: "Recieved data successfully"
-    });
+    const data = req.body;
+    const newSignUp = new SignUp(data);
+    // Sending and saving the data recieved from frontend to MongoDb
+    newSignUp.save((error) =>{
+        if(error)
+            res.status(500).json({
+                msg: "OOPS, Something went Wrong"
+            });
+        else
+            res.json({
+                msg: "New user data has been saved successfully"
+            });
+
+    })
+    console.log("Body: ", data);
 });
 
 module.exports = router;
