84ca94eef84877e921606a6a89b071a10eea88f8
pranavsoni1101 <pranavsoni1101@gmail.com>
2021-06-28 15:52:36 +0530

    Seeding the database with products

---
+++ backend/seedProducts.js
@@ -0,0 +1,84 @@
+const products = [
+    {
+        classification: "medicine",
+        productName: "Disprin",
+        packSize: "10",
+        manufacturer: "Reckitt & Colman of India Ltd",
+        typeOfProduct: "Dispersible Tablet",
+        price: "11.20"
+    },
+    {
+        classification: "medicine",
+        productName: "Evion 400mg",
+        packSize: "10",
+        manufacturer: "Merck Limited",
+        typeOfProduct: "Capsule",
+        price: "32.70"
+    },
+    {
+        classification: "medicine",
+        productName: "Dolo 650mg",
+        packSize: "15",
+        manufacturer: "Micro Labs",
+        typeOfProduct: "Tablet",
+        price: "30.91"
+    },
+    {
+        classification: "healthcare",
+        productName: "Horlicks Classic Malt Nutritio Drink",
+        manufacturer: "Horlicks",
+        typeOfProduct: "Refil",
+        packSize: "500 gm",
+        price: "229"
+    },
+    {
+        classification: "medicine",
+        productName: "Zincovit",
+        packSize: "15",
+        manufacturer: "Apex Laboratories Private Limited",
+        typeOfProduct: "Tablet",
+        price: "105"
+    },
+    {
+        classification: "medicine",
+        productName: "Limcee 500mg Orange Flavour",
+        packSize: "15",
+        manufacturer: "Abbott Healthcare Pvt Ltd",
+        typeOfProduct: "Chewable Tablet",
+        price: "23.05"
+    },
+    {
+        classification: "medicine",
+        productName: "Amoxyclav 625mg",
+        packSize: "10",
+        manufacturer: "Abbott Healthcare Pvt Ltd",
+        typeOfProduct: "Chewable Tablet",
+        price: "200.50"
+    },
+    {
+        classification: "healthcare",
+        productName: "Parachute 100% Pure Coconut Oil",
+        manufacturer: "Parachute",
+        typeOfProduct: "Bottle",
+        packSize: "600 ml",
+        price: "261"
+    },
+    {
+        classification: "healthcare",
+        productName: "Colgate Strong Teeth",
+        manufacturer: "Colgate",
+        typeOfProduct: "Tooth Paste",
+        packSize: "46 gm",
+        price: "20"
+    },
+    {
+        classification: "healthcare",
+        productName: "Gillette Classic Lemon Lime",
+        manufacturer: "Horlicks",
+        typeOfProduct: "Foam Bottle",
+        packSize: "196 gm",
+        price: "229"
+    },
+];
+
+module.exports = products;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const routes = require('./routes/api');
 const connectDb = require('./db');
+const mongoose = require('mongoose');
+const products = require('./seedProducts');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const Schema = mongoose.Schema;
 
+const ProductSchema = new Schema({
+    classification: String,
+    productName: String,
+    manufacturer: String,
+    typeOfProduct: String,
+    packSize: String,
+    price: String 
+
+});
+const Product = mongoose.model('products', ProductSchema);
+
+// -----------------Seeding database with Products-----------------
+// Product.insertMany(products, (error) =>{
+//     if(error)
+//         console.log("Error:", error);
+//     console.log("Savedd products");
+// });
+// ----------------------------------------------------------------
+
+// Establishing connection with the databse
 connectDb();
 
 app.use(express.json());
